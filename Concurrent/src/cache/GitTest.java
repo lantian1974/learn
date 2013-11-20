57575a7e36e61eab0f653e7b8a090c6992c804bc
lantian1974 <ltian.cn@gmail.com>
2013-11-20 16:33:45 +0800

    sdf

---
+++ Concurrent/src/cache/GitTest.java
@@ -0,0 +1,13 @@
+package cache;
+
+public class GitTest 
+{
+   public static void main(String[] args)
+   {
+	    sayHello();
+   }
+   public static void sayHello()
+   {
+	   System.out.println("Hello World!"); 
+   }
+}
