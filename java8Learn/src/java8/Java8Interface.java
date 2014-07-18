5e2c0ea7e11c267e78ab043938853eff2a702ccd
lantian1974 <ltian.cn@gmail.com>
2014-07-18 22:09:59 +0800

    add all dir

---
+++ java8Learn/src/java8/Java8Interface.java
@@ -0,0 +1,9 @@
+package java8;
+
+public interface Java8Interface 
+{
+  default void sayHello(String name)
+  {
+	  System.out.println("Hello "+name+" ! ");
+  }
+}
---
+++ java8Learn/src/java8/TestJava8Interface.java
@@ -0,0 +1,10 @@
+package java8;
+
+public class TestJava8Interface implements Java8Interface
+{
+   public static void main(String[] args)
+   {
+	   Java8Interface instance=new  TestJava8Interface();
+	   instance.sayHello("every one" ); 
+   }
+}
